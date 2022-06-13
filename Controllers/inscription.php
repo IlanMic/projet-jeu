08c00794e6f17687337c04d3274c35e16af2b111
IlanMic <ilan.micheau-maillou@groupe-esigelec.org>
2022-06-13 12:02:32 +0200

    Ajout des fonctions isset pour vérifier la déclaration des variables
    Cela permet de vérifier que des variables ont bien été saisies et qu'elles ne valent pas "null"

--- Controllers/inscription.php
+++ Controllers/inscription.php
@@ -1,43 +1,48 @@
-<?php
+﻿<?php
     session_start();
-    //Convertit les caractères éligibles en format html
-    $pseudo =  htmlentities($_POST['pseudo']);
-    $mail = htmlentities($_POST['mail']);
-    $pass =  htmlentities($_POST['pass']);
-    $options = [
-        'cost' => 12,
-    ]; 
 
-    require_once("../Core/ConnexionBDD.php");
-    $pdo = connect_db();
+    //Vérification de la déclaration des variables
+    if(isset($_POST['pseudo']) && isset($_POST['mail']) && isset($_POST['pass']) && isset($_POST['premium'])) {
 
-    //On vérifie la volonté de créer un compte premium ou non
-    $premium = $_POST['premium'];
-    echo $premium;
-    if($premium == 1) {
-        $premium = 1;
-    }else{
-        $premium = 0;
+        //Récupération des variables du formulaire
+        $pseudo = $_POST['pseudo'];
+        $mail = $_POST['mail'];
+        $pass =  $_POST['pass'];
+        $premium = $_POST['premium'];
+        $options = [
+            'cost' => 12,
+        ]; 
+
+        //Connexion à la base de données
+        require_once("../Core/ConnexionBDD.php");
+        $pdo = connect_db();
+
+        //Vérification de la volonté de créer un compte premium ou non
+        if($premium == 1) {
+            $premium = 1;
+        }else{
+            $premium = 0;
+        }
+
+        //Insertion de l'utilisateur dans la base de données
+        $stmt = $pdo->prepare("INSERT INTO utilisateur (pseudo, adresse_mail, mot_de_passe, compte_premium) VALUES (:pseudo, :mail, :mdp, :premium)");
+        $hash_mdp = password_hash($pass, PASSWORD_BCRYPT, $options);
+        $stmt->bindParam("pseudo", $pseudo, PDO::PARAM_STR);
+        $stmt->bindParam("mail", $mail, PDO::PARAM_STR);
+        $stmt->bindParam("mdp", $hash_mdp, PDO::PARAM_STR);
+        $stmt->bindParam("premium", $premium, PDO::PARAM_INT);
+        if($stmt->execute()) {
+            $_SESSION['etat'] = "Succès";
+            $_SESSION['message'] = "Inscription réussie";
+            header('Location: ../Views/index.php');
+
+        } else {
+            $_SESSION['etat'] = "Echec";
+            $_SESSION['message'] =  "Erreur: au moins une saisie ne remplit pas les conditions nécessaires.";
+            header('Location: ../Views/index.php');
+        }
+
     }
-
-    //Insertion de l'utilisateur dans la base de données
-    $stmt = $pdo->prepare("INSERT INTO utilisateur (pseudo, adresse_mail, mot_de_passe, compte_premium) VALUES (:pseudo, :mail, :mdp, :premium)");
-    $hash_mdp = password_hash($pass, PASSWORD_BCRYPT, $options);
-    $stmt->bindParam("pseudo", $pseudo, PDO::PARAM_STR);
-    $stmt->bindParam("mail", $mail, PDO::PARAM_STR);
-    $stmt->bindParam("mdp", $hash_mdp, PDO::PARAM_STR);
-    $stmt->bindParam("premium", $premium, PDO::PARAM_INT);
-    if($stmt->execute()) {
-        $_SESSION['etat'] = "Succès";
-        $_SESSION['message'] = "Inscription réussie";
-        header('Location: ../Views/index.php');
-
-    } else {
-        $_SESSION['etat'] = "Echec";
-        $_SESSION['message'] =  "Erreur !";
-        header('Location: ../Views/index.php');
-    }
-
 
     
 
