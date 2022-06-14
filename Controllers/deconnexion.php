2638ad181831440bd0d0262ff455616107a5c3f8
Ilan Micheau <ilan.micheau-maillou@groupe-esigelec.org>
2022-06-14 13:38:05 +0200

    Mise à jour du système de déconnexion
    Plusieurs variables de sessions sont annulées

--- Controllers/deconnexion.php
+++ Controllers/deconnexion.php
@@ -1,5 +1,18 @@
 <?php
     session_start();
+    //Définition du statut de connexion à faux
     $_SESSION['statut_connexion'] = false;
+
+    //On supprime les variables de la session
+    unset($_SESSION['utilisateur_id']);
+    unset($_SESSION['utilisateur_pseudo']);
+    unset($_SESSION['mail']);
+    unset($_SESSION['utilisateur_club']);
+    unset($_SESSION['compte_premium']);
+    unset($_SESSION['utilisateur_personnage_1']);
+    unset($_SESSION['utilisateur_personnage_2']);
+    unset($_SESSION['utilisateur_personnage_3']);
+
+    //Redirection vers la page d'accueil
     header('Location: ../Views/index.php');
 ?>
