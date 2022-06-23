15dc2471fa08551379e359523dc6df080ff66ab7
Ilan Micheau <ilan.micheau-maillou@groupe-esigelec.org>
2022-06-23 16:04:27 +0200

    Impossible de créer un club si l'utilisateur en a déjà créé un

--- Views/creation-club.php
+++ Views/creation-club.php
@@ -1,16 +1,31 @@
 <!-- Header/Navbar -->
-<?php
+<?php    
+
     $titre_page = " Création du Club";
     include dirname(dirname(__FILE__)) .'/Views/layout/header.php';
     include dirname(dirname(__FILE__)) .'/Views/layout/navbar.php';
     require_once("../Core/Core.php");
     redirection_si_non_connecte($_SESSION['statut_connexion']);
+    require_once("../Models/utilisateur.php");
+    if(!empty(get_club_nom($_SESSION['utilisateur_id']))){
+        ?>
+            <script type="text/javascript">
+            window.location.href = '../Views/index.php';
+            </script>
+        <?php
+    }
+
 ?>
 
 <body>
+<div class="alert-container">
+    <?php
+        require_once("../Views/layout/message.php");
+    ?>
+</div>
 <!-- Container contenant les informations pour la création du club -->
 <div class="container-creation">
-    <form class="create-club-form" action="Controllers/create-club.php">
+    <form class="create-club-form" action="../Controllers/creer-club.php" method="POST">
         <div class="label-connexion-head">
             <h1>Sélection du nom du club</h1>
             <hr>
@@ -20,8 +35,8 @@
         <div class="club-edit-content">
             <div class="row">
                 <div class="mb-4">
-                    <label for="nom-club" class="form-label">Nom du club *:</label><br>
-                    <input type="text" id="nom-club" class="form-control" name="nom-club" placeholder="Veuillez saisir le nom du club..." required>
+                    <label for="nom_club" class="form-label">Nom du club *:</label><br>
+                    <input type="text" id="nom_club" class="form-control" name="nom_club" placeholder="Veuillez saisir le nom du club..." required>
                 </div>
                 <div class="text-center mt-4 mb-3">
                     <button type="submit" name="submit" class="form-btn">Créer</button>
