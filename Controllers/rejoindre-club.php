d1365df984424791fbd11e7d85a303b8b0d4c5fb
Ilan Micheau <ilan.micheau-maillou@groupe-esigelec.org>
2022-06-27 16:01:48 +0200

    Ajout du controller pour qu'un  personnage rejoigne un club

---
+++ Controllers/rejoindre-club.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+
+    if(isset($_POST['personnageid'])) {
+        $identifiant_club = $_POST['club_id'];
+        $identififiant_personnage = $_POST['personnageid'];
+        require_once("../Core/ConnexionBDD.php");
+        $pdo = connect_db();
+        $stmt = $pdo->prepare("UPDATE personnage SET club_id = :club_id WHERE id_personnage = :id_personnage");
+        $stmt->bindParam("club_id", $identifiant_club, PDO::PARAM_INT);
+        $stmt->bindParam("id_personnage", $identififiant_personnage, PDO::PARAM_INT);
+        if($stmt->execute()) {
+            $_SESSION['etat'] = "Succès";
+            $_SESSION['message'] = "Le personnage a bien rejoint le club.";
+            header('Location: ../Views/index.php');
+        } else {
+            $_SESSION['etat'] = "Echec";
+            $_SESSION['message'] = "Le personnage ne peut rejoindre le club";
+            header('Location: ../Views/index.php');
+        }
+    } else{
+        $_SESSION['etat'] = "Echec";
+        $_SESSION['message'] = "Veuillez remplir tous les champs obligatoires pour que le personnage puiss rejoindre le club.";
+        header('Location: ../Views/index.php');
+    }
+?>
