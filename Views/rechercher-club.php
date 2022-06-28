bee7c83c8eabb029f209aea01c1b0c07432cff16
Ilan Micheau <ilan.micheau-maillou@groupe-esigelec.org>
2022-06-28 15:54:22 +0200

    Problème résolu: id de club n'allant pas dans le modal

--- Views/rechercher-club.php
+++ Views/rechercher-club.php
@@ -51,80 +51,93 @@ if(isset($_POST['nom_club'])) {
             } else {
                 $poule_nom = "Ce club n'a pas encore rejoint une division.";
             }
-        ?>
-        <div class="card" id="card-2">
-            <div class="row no-gutters">
-                <div class="card-body thin-margin">
-                    <h5 class="card-title"><a href="profil-club.php?c=<?php echo $club_id ?>"><?php echo $club['nom_club']?></a></h5>
-                    <hr>
-                    <div class="row">
-                        <div class="col-sm-7">
-                            <br>
-                            <p class="card-text">Propriétaire: <a href="profil-utilisateur.php?u=<?php echo $club['proprietaire_id'] ?>"><?php echo get_pseudo($club['proprietaire_id']) ?></a></p>
-                            <br>
-                            <br>
-                            <p class="card-text">Nombre de joueurs : <?php echo compter_personnages_club($club['id_club']) ?>  </p>
-                            <br>
-                            <br>
-                            <p class="card-text">Division actuelle: <?php echo $poule_nom ?></p>
-                        </div>
-                        <div class="col-sm-5">
-                            <div class ="join-club-btn">
-                                <!-- Button trigger modal -->
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#join-club">
-                                Rejoindre ce club ?
-                                </button>
-                                <!-- Formulaire pour que le personnage sélectionné rejoigne le club -->
-                                <form action="../Controllers/rejoindre-club.php" method="POST">
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="join-club" tabindex="-1" role="dialog" aria-labelledby="Rejoindre-ce-club" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Séléctionnez le personnage qui rejoindra ce club</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <div class="form-check">
-                                                <input class="form-check-input" type="radio" name="personnageid" id="personnageid" value="<?php echo $_SESSION['utilisateur_personnage_1'] ?>" checked>
-                                                <label class="form-check-label" for="personnage-1">
-                                                    <?php echo get_nom_personnage($_SESSION['utilisateur_personnage_1']); ?>
-                                                </label>
+            ?>
+            <div class="card" id="card-2">
+                <div class="row no-gutters">
+                    <div class="card-body thin-margin">
+                        <h5 class="card-title"><a class="a-title" href="profil-club.php?c=<?php echo $club_id ?>"><?php echo $club['nom_club']?></a></h5>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-7">
+                                <br>
+                                <p class="card-text">Propriétaire: <a class="a-title" href="profil-utilisateur.php?u=<?php echo $club['proprietaire_id'] ?>"><?php echo get_pseudo($club['proprietaire_id']) ?></a></p>
+                                <br>
+                                <br>
+                                <p class="card-text">Nombre de joueurs : <?php echo compter_personnages_club($club['id_club']) ?>  </p>
+                                <br>
+                                <br>
+                                <p class="card-text">Division actuelle: <?php echo $poule_nom ?></p>
+                            </div>
+                            <div class="col-sm-5">
+                                <div class ="join-club-btn">
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#join-club-<?php echo $club_id; ?>">
+                                    Rejoindre ce club ?
+                                    </button>
+                                    <!-- Formulaire pour que le personnage sélectionné rejoigne le club -->
+                                    <form action="../Controllers/rejoindre-club.php" method="POST">
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="join-club-<?php echo $club_id; ?>" tabindex="-1" role="dialog" aria-labelledby="Rejoindre-ce-club" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel">Séléctionnez le personnage qui rejoindra ce club.</h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <?php
+                                                        if($_SESSION['utilisateur_personnage_1'] != null){
+                                                            ?>
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="radio" name="personnageid" id="personnageid" value="<?php echo $_SESSION['utilisateur_personnage_1'] ?>" checked>
+                                                                <label class="form-check-label" for="personnage-1">
+                                                                    <?php echo get_nom_personnage($_SESSION['utilisateur_personnage_1']); ?>
+                                                                </label>
+                                                            </div>
+                                                            <?php
+                                                        } 
+                                                        if($_SESSION['utilisateur_personnage_2'] != null) {
+                                                            ?>
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="radio" name="personnageid" id="personnageid" value="<?php echo $_SESSION['utilisateur_personnage_2'] ?>">
+                                                                <label class="form-check-label" for="personnage-2">
+                                                                    <?php echo get_nom_personnage($_SESSION['utilisateur_personnage_2']); ?>
+                                                                </label>
+                                                            </div>
+                                                            <?php
+                                                        }
+                                                        if($_SESSION['utilisateur_personnage_3'] != null){
+                                                            ?>
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="radio" name="personnageid" id="personnageid" value="<?php echo $_SESSION['utilisateur_personnage_3'] ?>">
+                                                                <label class="form-check-label" for="personnage-3">
+                                                                    <?php echo get_nom_personnage($_SESSION['utilisateur_personnage_3']); ?>
+                                                                </label>
+                                                            </div>
+                                                            <?php
+                                                        }
+                                                        ?>
+                                                        <input type="hidden" name="club_id" id="club_id" value="<?php echo $club_id; ?>">
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                                                        <button type="submit" class="btn btn-primary">Rejoindre le club avec ce personnage</button>
+                                                    </div>
                                                 </div>
-                                                <div class="form-check">
-                                                <input class="form-check-input" type="radio" name="personnageid" id="personnageid" value="<?php echo $_SESSION['utilisateur_personnage_2'] ?>">
-                                                <label class="form-check-label" for="personnage-2">
-                                                    <?php echo get_nom_personnage($_SESSION['utilisateur_personnage_2']); ?>
-                                                </label>
-                                                </div>
-                                                <div class="form-check">
-                                                <input class="form-check-input" type="radio" name="personnageid" id="personnageid" value="<?php echo $_SESSION['utilisateur_personnage_3'] ?>">
-                                                <label class="form-check-label" for="personnage-3">
-                                                    <?php echo get_nom_personnage($_SESSION['utilisateur_personnage_3']); ?>
-                                                </label>
-                                                <input type="hidden" name="club_id" id="club_id" value="<?php echo $club["id_club"]; ?>">
-                                            </div>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                                                <button type="submit" class="btn btn-primary">Rejoindre le club avec ce personnage</button>
-                                            </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </form>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <?php
+            <?php
+        }
     }
-    }
-
 }
 ?>
 
