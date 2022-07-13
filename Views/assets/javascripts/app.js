78dffe1cd17fa8a1718970fa22499b9a472a8cd9
Ilan Micheau <ilan.micheau-maillou@groupe-esigelec.org>
2022-07-13 16:04:23 +0200

    Javascript pour limiter le nombre de joueur sélectionné dans la composition à 6, toute position confondue

--- Views/assets/javascripts/app.js
+++ Views/assets/javascripts/app.js
@@ -194,5 +194,26 @@ $(document).ready(function () {
         });
     }
 
+    var max = 6;
+    var $inputs = $('input');
+    
+    function sumInputs($inputs) {
+      var sum = 0;
+      
+      $inputs.each(function() {
+        sum += parseInt($(this).val(), 0);
+      });
+    
+      return sum;
+    }
+    
+    $inputs.on('input', function(e) {
+      var $this = $(this);
+      var sum = sumInputs($inputs.not(function(i, el) {
+        return el === e.target;
+      }));
+      var value = parseInt($this.val(), 10) || 0;
+      if(sum + value > max) $this.val(max - sum);
+    });
 
 });
