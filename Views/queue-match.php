21631bf89f3c3c3909defdd3acf102ba358d2d5d
Ilan Micheau <ilan.micheau-maillou@groupe-esigelec.org>
2022-07-20 16:15:22 +0200

    Modification de la page d'attente

--- Views/queue-match.php
+++ Views/queue-match.php
@@ -5,12 +5,48 @@
     include dirname(dirname(__FILE__)) .'/Views/layout/navbar.php';
     require_once($_SERVER['DOCUMENT_ROOT']. "projet-jeu/Models/personnage.php");
     require_once($_SERVER['DOCUMENT_ROOT']. "projet-jeu/Models/utilisateur.php");
+    require_once($_SERVER['DOCUMENT_ROOT']. "projet-jeu/Models/match.php");
+    if(isset($_SESSION['match_en_attente']) && $_SESSION['match_en_attente'] == true) {
+        if(!isset($_SESSION['opposant']) || $_SESSION['opposant'] == null ) {
+            ?>
+            <meta http-equiv="refresh" content="30">
+            <?php
+            $match_cree = $_SESSION['match_en_attente_id'];
+            if(get_utilisateur_1($match_cree) != $_SESSION['dernier_personnage_utilise'] && get_utilisateur_1($match_cree) != null) {
+                $_SESSION['opposant'] = get_utilisateur_1($match_cree);
+                $nom_opposant = get_pseudo($_SESSION['opposant']);
+                $capitaine_opposant = get_dernier_personnage($_SESSION['opposant']);
+                $illustration_capitaine_opposant = get_illustration($_SESSION['opposant']);
+            } else if(get_utilisateur_2($match_cree) != $_SESSION['dernier_personnage_utilise'] && get_utilisateur_2($match_cree) != null) {
+                $_SESSION['opposant'] = get_utilisateur_2($match_cree);
+                $nom_opposant = get_pseudo($_SESSION['opposant']);
+                $capitaine_opposant = get_dernier_personnage($_SESSION['opposant']);
+                $illustration_capitaine_opposant = get_illustration($_SESSION['opposant']);                
+            } else {
+                $nom_opposant = "En attente d'adversaire";
+                $capitaine_opposant = null;
+                $illustration_capitaine_opposant = null;
+            }
+        } else {
+            $nom_opposant = get_pseudo($_SESSION['opposant']);
+            $capitaine_opposant = get_dernier_personnage($_SESSION['opposant']);
+            $illustration_capitaine_opposant = get_illustration($_SESSION['opposant']);
+        }
+    } else if(isset($_SESSION['match_en_cours']) && $_SESSION['match_en_cours'] == true) {
+        $nom_opposant = get_pseudo($_SESSION['opposant_match']);
+        $capitaine_opposant = get_dernier_personnage($_SESSION['opposant_match']);
+        $illustration_capitaine_opposant = get_illustration($_SESSION['opposant_match']);
+    } else {
+        $nom_opposant = "En attente d'adversaire";
+        $capitaine_opposant = null;
+        $illustration_capitaine_opposant = null;
+    }
 ?>
 
 <body>
 <div class="queue-form-container">
     <!-- Formulaire de fil d'attente -->
-    <form class="creation-form" action="Controllers/queue.php">
+    <div class="creation-form">
         <div class="label-connexion-head">
             <h1>Fil d'attente</h1>
             <hr>
@@ -28,7 +64,7 @@
                             $pseudo_utilisateur = "guest_".rand(1111,9999);
                         }
                         if($_SESSION['statut_connexion'] == true && isset($_SESSION['dernier_personnage_utilise']) && $_SESSION['dernier_personnage_utilise'] != null && get_illustration($_SESSION['dernier_personnage_utilise']) != null) {
-                            echo '<img class="card-img-top img-illustration" src="data:image/jpeg;base64,'.base64_encode(($_SESSION['dernier_personnage_utilise'])).'" alt="Profil du personnage 1"/>';
+                            echo '<img class="card-img-top img-illustration" src="data:image/jpeg;base64,'.base64_encode(get_illustration($_SESSION['dernier_personnage_utilise'])).'" alt="Profil du personnage 1"/>';
                         } else {
                             ?>
                             <img class="card-img-top" src="assets/images/illustration-placeholder.png" alt="Personnage utilisé par <?php echo $pseudo_utilisateur ?>">
@@ -44,18 +80,29 @@
                 </div>                
                 <div class="col-sm d-flex justify-content-center">
                     <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="assets/images/illustration-placeholder.png" alt="Personnage utilisé par _pseudo_">
+                        <?php
+                        if(isset($_SESSION['match_en_cours']) && $_SESSION['match_en_cours'] == true && $illustration_capitaine_opposant == null) {
+                            echo  '<img class="card-img-top" src="assets/images/illustration-placeholder.png" alt="Personnage utilisé par <?php echo $nom_opposant ?>">';
+                        } else if(isset($_SESSION['match_en_cours'])  && $_SESSION['match_en_cours'] == true && $illustration_capitaine_opposant != null) {
+                            echo '<img class="card-img-top img-illustration" src="data:image/jpeg;base64,'.base64_encode($illustration_capitaine_opposant).'" alt="Profil du capitaine de "/>';
+                        } else {
+                            echo '<img class="card-img-top" src="assets/images/illustration-placeholder.png" alt="Image de profil">';
+                        }
+                        ?>
                         <div class="card-body left">
-                            <p class="card-text">Pseudo utilisateur</p>
+                            <p class="card-text"><?php echo $nom_opposant ?></p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="text-center mt-4 mb-3">
-                <button type="submit" name="submit" class="form-btn">Quitter la file d'attente</button>
-            </div>
+            <form action="../Controllers/Jeu/quitter-queue.php" method="POST">
+                <div class="text-center mt-4 mb-3">
+                    <button type="submit" name="submit" class="form-btn">Quitter la file d'attente</button>
+                </div>
+            </form>
+
         </div>
-    </form>
+    </div>
 </div>
 
 <!-- Footer -->
