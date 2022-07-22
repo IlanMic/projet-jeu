7a92acc95f122bae1af46784b1f214b5ca48f5b8
Ilan Micheau <ilan.micheau-maillou@groupe-esigelec.org>
2022-07-22 16:38:51 +0200

    Vérification que l'adresse mail et le pseudo saisis sont bien uniques

--- Controllers/inscription.php
+++ Controllers/inscription.php
@@ -14,38 +14,48 @@
         ]; 
         require_once($_SERVER['DOCUMENT_ROOT']. "projet-jeu/Core/Core.php");
         if(mot_de_passe_valide($pass)) {
-            //Connexion à la base de données
-            require_once($_SERVER['DOCUMENT_ROOT']. "projet-jeu/Core/ConnexionBDD.php");
-            $pdo = connect_db();
-
-            //Vérification de la volonté de créer un compte premium ou non
-            if($premium == 1) {
-                $premium = 1;
-            }else{
-                $premium = 0;
-            }
-
-            //Insertion de l'utilisateur dans la base de données
-            $stmt = $pdo->prepare("INSERT INTO utilisateur (pseudo, adresse_mail, mot_de_passe, compte_premium) VALUES (:pseudo, :mail, :mdp, :premium)");
-            $hash_mdp = password_hash($pass, PASSWORD_BCRYPT, $options);
-            $stmt->bindParam("pseudo", $pseudo, PDO::PARAM_STR);
-            $stmt->bindParam("mail", $mail, PDO::PARAM_STR);
-            $stmt->bindParam("mdp", $hash_mdp, PDO::PARAM_STR);
-            $stmt->bindParam("premium", $premium, PDO::PARAM_INT);
-            if($stmt->execute()) {
-                $_SESSION['etat'] = "Succès";
-                $_SESSION['message'] = "Inscription réussie. Vous pouvez désormais vous connecter";
-                header('Location: ../Views/index.php');
-
+            if(pseudo_est_unique($pseudo)) {
+                if(mail_est_unique($mail)) {
+                    //Connexion à la base de données
+                    require_once($_SERVER['DOCUMENT_ROOT']. "projet-jeu/Core/ConnexionBDD.php");
+                    $pdo = connect_db();
+    
+                    //Vérification de la volonté de créer un compte premium ou non
+                    if($premium == 1) {
+                        $premium = 1;
+                    }else{
+                        $premium = 0;
+                    }
+    
+                    //Insertion de l'utilisateur dans la base de données
+                    $stmt = $pdo->prepare("INSERT INTO utilisateur (pseudo, adresse_mail, mot_de_passe, compte_premium) VALUES (:pseudo, :mail, :mdp, :premium)");
+                    $hash_mdp = password_hash($pass, PASSWORD_BCRYPT, $options);
+                    $stmt->bindParam("pseudo", $pseudo, PDO::PARAM_STR);
+                    $stmt->bindParam("mail", $mail, PDO::PARAM_STR);
+                    $stmt->bindParam("mdp", $hash_mdp, PDO::PARAM_STR);
+                    $stmt->bindParam("premium", $premium, PDO::PARAM_INT);
+                    if($stmt->execute()) {
+                        $_SESSION['etat'] = "Succès";
+                        $_SESSION['message'] = "Inscription réussie. Vous pouvez désormais vous connecter";
+                        header('Location: ../Views/index.php');
+    
+                    } else {
+                        $_SESSION['etat'] = "Echec";
+                        $_SESSION['message'] =  "Erreur: au moins une saisie ne remplit pas les conditions nécessaires.";
+                        header('Location: ../Views/index.php');
+                    }
+                    //Fermeture connexion base de données
+                    $pdo = null;
+                } else {
+                    $_SESSION['etat'] = "Echec";
+                    header('Location: ../Views/index.php');
+                    $_SESSION['message'] = "Un utilisateur utilise déjà cette adresse mail.";
+                }
             } else {
                 $_SESSION['etat'] = "Echec";
-                $_SESSION['message'] =  "Erreur: au moins une saisie ne remplit pas les conditions nécessaires.";
                 header('Location: ../Views/index.php');
+                $_SESSION['message'] = "Un utilisateur utilise déjà ce pseudo.";                
             }
-
-
-            //Fermeture connexion base de données
-            $pdo = null;
         } else {
             $_SESSION['etat'] = "Echec";
             header('Location: ../Views/index.php');
